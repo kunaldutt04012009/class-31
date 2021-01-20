01a0db8f88118bd6492836722ea4de3762cf777b
kunaldutt04012009 <75310184+kunaldutt04012009@users.noreply.github.com>
2021-01-20 18:57:58 +0530

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,40 @@
+var ob1="kunal"
+console.log(ob1);
+console.log(typeof ob1);
+
+var num=100;
+console.log(num);
+console.log(typeof num);
+
+var num2="100"
+console.log(num2);
+console.log(typeof num2);
+
+var ob2=true
+console.log(ob2);
+console.log(typeof ob2);
+
+var object={
+    "name":"kunal",
+    "age":12
+}
+console.log(object);
+console.log(typeof object);
+
+var arr1=[10,20,30,40,50];
+console.log(arr1);
+console.log(typeof arr1);
+console.log(arr1.length);
+console.log(arr1[3]);
+
+var arr2=["kunal",true,30.5];
+console.log(arr2);
+console.log(arr2[0]);
+
+var arr3=[[10,20],[30,40,50],[55,66]];
+console.log(arr3);
+console.log(arr3[1][2]);
+console.log(arr3[0][1]);
+
+arr3.push([88,99]);
+console.log(arr3);
